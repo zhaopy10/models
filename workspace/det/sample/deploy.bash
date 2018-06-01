d023209abf889b4fbb8cc5a91ce03484efd8040a
xuyithu <xuyithu@gmail.com>
2018-06-01 11:48:06 +0800

    Cleaned up deploy.bash

--- workspace/det/sample/deploy.bash
+++ workspace/det/sample/deploy.bash
@@ -12,60 +12,29 @@ CKPT="${WORKSPACE}/coco_ckpt/model.ckpt-34205"
 DEPLOY="${TRAIN_DIR}/deploy"
 mkdir -p ${DEPLOY}
 
-#pb_path="${DEPLOY}/saved_model/saved_model.pb"
-#log_dir="${DEPLOY}/saved_model/"
-#pbtxt_name="saved_model.pbtxt"
-pb_path="${DEPLOY}/frozen_inference_graph.pb"
-pbtxt_name="frozen_inference_graph.pbtxt"
+deploy_graph_name="deploy_graph"
+deploy_input_nodes="Preprocessor/sub:0"
+deploy_output_nodes="box_encodings:0,class_indices:0,score_sigmoids:0"
+ml_input_nodes=${deploy_input_nodes}
+#ml_output_nodes=${deploy_output_nodes}
+ml_output_nodes="all"
 
-input_node="image_tensor:0"
-output_node="box_encodings:0,class_indices:0,score_sigmoids:0,anchors:0"
-
-#######################################
-####### export for tf inference #######
-#input_node="image_tensor:0"
-#output_node="detection_boxes:0,detection_scores:0,detection_classes:0,num_detections:0"
-#
-#python ${TFDET}/export_inference_graph.py \
-#    --input_type="image_tensor" \
-#    --pipeline_config_path=${PIPELINE_CONFIG} \
-#    --trained_checkpoint_prefix=${CKPT} \
-#    --output_directory=${DEPLOY}
-#
-#python ${WORKSPACE}/../my_graph_utils/pb2pbtxt.py \
-#    --pb_path=${pb_path} \
-#    --log_dir=${DEPLOY} \
-#    --pbtxt_name=${pbtxt_name}
-########################################
-
-##################################
-###### deploy for mobile app ######
-
+###### deploy for mobile app, no pre- or post-processing ######
+echo "Exporting CoreML model..."
+pretransform_graph_name="pretransform_graph"
 python ${TFDET}/export_inference_graph.py \
     --input_type="image_tensor" \
     --pipeline_config_path=${PIPELINE_CONFIG} \
     --trained_checkpoint_prefix=${CKPT} \
     --output_directory=${DEPLOY} \
-    --deploy=True \
-    --output_anchors=True
-
-python ${WORKSPACE}/../my_graph_utils/pb2pbtxt.py \
-    --pb_path=${pb_path} \
-    --log_dir=${DEPLOY} \
-    --pbtxt_name=${pbtxt_name}
-
-
-input_node="Preprocessor/sub:0"
-output_node="box_encodings:0,class_indices:0,score_sigmoids:0"
-#ml_output_node=${output_node}
-ml_input_node="Preprocessor/sub:0"
-ml_output_node="all"
+    --output_graph_name="${pretransform_graph_name}.pb" \
+    --deploy=True
 
 ${TFBAZEL}/tensorflow/tools/graph_transforms/transform_graph \
-    --in_graph=${DEPLOY}/frozen_inference_graph.pb \
-    --out_graph=${DEPLOY}/deploy_graph.pb \
-    --inputs=${input_node} \
-    --outputs=${output_node} \
+    --in_graph="${DEPLOY}/${pretransform_graph_name}.pb" \
+    --out_graph="${DEPLOY}/${deploy_graph_name}.pb" \
+    --inputs=${deploy_input_nodes} \
+    --outputs=${deploy_output_nodes} \
     --transforms='strip_unused_nodes(type=float, shape="1,300,300,3")
                   remove_nodes(op=Identity, op=CheckNumerics)
                   fold_constants(ignore_errors=true)
@@ -73,16 +42,74 @@ ${TFBAZEL}/tensorflow/tools/graph_transforms/transform_graph \
                   fold_old_batch_norms'
 
 python ${WORKSPACE}/../my_graph_utils/pb2pbtxt.py \
-    --pb_path=${DEPLOY}/deploy_graph.pb \
+    --pb_path="${DEPLOY}/${deploy_graph_name}.pb" \
     --log_dir=${DEPLOY} \
-    --pbtxt_name=${DEPLOY}/deploy_graph.pbtxt
+    --pbtxt_name="${deploy_graph_name}.pbtxt"
 
 python ${WORKSPACE}/../tf_coreml_utils/tf2coreml.py \
-    --input_pb_file=${DEPLOY}/deploy_graph.pb \
-    --output_mlmodel=${DEPLOY}/deploy_graph.mlmodel \
-    --input_node_name=${ml_input_node} \
-    --output_node_name=${ml_output_node}
+    --input_pb_file="${DEPLOY}/${deploy_graph_name}.pb" \
+    --output_mlmodel="${DEPLOY}/${deploy_graph_name}.mlmodel" \
+    --input_node_name=${ml_input_nodes} \
+    --output_node_name=${ml_output_nodes}
 
+echo ""
+echo "Done with exporting CoreML model!!!"
+echo ""
+###############################################################
+
+
+##################################################
+###### export graph without post-processing ######
+echo "Exporting prepost graph..."
+#tf_input_node="image_tensor:0"
+#output_node="box_encodings:0,class_indices:0,score_sigmoids:0,anchors:0"
+prepost_graph_name="prepost_graph"
+PREPOST_DEPLOY="${DEPLOY}/prepost"
+
+python ${TFDET}/export_inference_graph.py \
+    --input_type="image_tensor" \
+    --pipeline_config_path=${PIPELINE_CONFIG} \
+    --trained_checkpoint_prefix=${CKPT} \
+    --output_directory=${PREPOST_DEPLOY} \
+    --output_graph_name="${prepost_graph_name}.pb" \
+    --deploy=True \
+    --output_anchors=True
+
+python ${WORKSPACE}/../my_graph_utils/pb2pbtxt.py \
+    --pb_path="${PREPOST_DEPLOY}/${prepost_graph_name}.pb" \
+    --log_dir=${PREPOST_DEPLOY} \
+    --pbtxt_name="${prepost_graph_name}.pbtxt"
+
+echo ""
+echo "Done with exporting prepost model!!!"
+echo ""
+##################################################
+
+
+######################################
+###### export for tf inference #######
+echo "Exporting tf graph for inference..."
+#tf_input_node="image_tensor:0"
+#tfoutput_node="detection_boxes:0,detection_scores:0,detection_classes:0,num_detections:0"
+tf_graph_name="tf_inference_graph"
+TF_DEPLOY="${DEPLOY}/tf_inference"
+
+python ${TFDET}/export_inference_graph.py \
+    --input_type="image_tensor" \
+    --pipeline_config_path=${PIPELINE_CONFIG} \
+    --trained_checkpoint_prefix=${CKPT} \
+    --output_directory=${TF_DEPLOY} \
+    --output_graph_name="${tf_graph_name}.pb"
+
+python ${WORKSPACE}/../my_graph_utils/pb2pbtxt.py \
+    --pb_path="${TF_DEPLOY}/${tf_graph_name}.pb" \
+    --log_dir=${TF_DEPLOY} \
+    --pbtxt_name="${tf_graph_name}.pbtxt"
+
+echo ""
+echo "Done with exporting tf inference model!!!"
+echo ""
+#######################################
 
 
 
