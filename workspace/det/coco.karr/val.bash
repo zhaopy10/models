56ef88940cc6e605345e9b58a5aa2098570502c1
xuyithu <xuyithu@gmail.com>
2018-06-04 16:05:14 +0800

    Updated det/coco.karr

---
+++ workspace/det/coco.karr/val.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+ROOT="/home/popzq/workspace/models"
+TFDET="${ROOT}/research/object_detection"
+WORKSPACE="${ROOT}/workspace/det/coco.512"
+PIPELINE_CONFIG="${WORKSPACE}/ssd_mnetv2_coco.config"
+TRAIN_DIR="${WORKSPACE}/train"
+
+python ${TFDET}/eval.py \
+    --logtostderr \
+    --pipeline_config_path=${PIPELINE_CONFIG} \
+    --checkpoint_dir=${TRAIN_DIR} \
+    --eval_dir=${TRAIN_DIR}
+
+
