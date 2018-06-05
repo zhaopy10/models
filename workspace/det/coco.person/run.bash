2154536b602c9eae34cf0d1292fa05c64f77a0f6
xuyithu <xuyithu@gmail.com>
2018-06-05 02:59:43 +0000

    Updated det/coco.person

--- workspace/det/coco.person/run.bash
+++ workspace/det/coco.person/run.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-ROOT="/home/corp.owlii.com/yi.xu/workspace/models"
+ROOT="/home/xuyithu/workspace/models"
 TFDET="${ROOT}/research/object_detection"
-WORKSPACE="${ROOT}/workspace/det/test"
+WORKSPACE="${ROOT}/workspace/det/coco.person"
 PIPELINE_CONFIG="${WORKSPACE}/ssd_mnetv2_coco.config"
 TRAIN_DIR="${WORKSPACE}/train"
+
+num_clones=4
 
 python ${TFDET}/train.py \
     --logtostderr \
     --pipeline_config_path=${PIPELINE_CONFIG} \
-    --train_dir=${TRAIN_DIR}
+    --train_dir=${TRAIN_DIR} \
+    --num_clones=${num_clones}
 
 
